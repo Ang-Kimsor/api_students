12eda8bb47ac72eac1b0bc6388edd2fdfa37a3fb
Kimsor <157135337+Kimsor1111@users.noreply.github.com>
2025-12-29 01:54:12 +0700

    add

--- db.php
+++ db.php
@@ -1,13 +1,14 @@
 <?php
 $pdo = new PDO(
-    "mysql:host=sql.freedb.tech;port=3306;dbname=freedb_student_api;charset=utf8mb4",
-    "freedb_Kimsor",
-    '$DRXMrMuUQNXW8E',
+    "mysql:host=sql12.freesqldatabase.com;dbname=sql12813112;charset=utf8mb4",
+    "sql12813112",
+    '8rzl7jfIW1',
     [
         PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
         PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC
     ]
 );
+
 // $pdo = new PDO(
 //     "mysql:host=localhost;dbname=api_for_teaching;charset=utf8mb4",
 //     "root",
